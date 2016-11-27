21de09509901f3d714387f916cfa2a3f8aac6929
David Aronsohn <WagThatTail@Me.com>
2016-11-26 23:57:30 -0800

    Up to FIFO

--- main.go
+++ main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"github.com/mitchellh/go-homedir"
+	"log"
 	"os"
+	"path/filepath"
 	"strconv"
+	"syscall"
 )
 
-const main_dir string = "~/muck/"
+const base_dir string = "muck"
+const in_file string = "in"
+const outfile string = "out"
 
 var (
 	connection_name   string
@@ -31,16 +37,14 @@ func init_vars() {
 
 	connection_args := flag.Args()
 	if len(connection_args) != 3 {
-		fmt.Println("Usage: mm [--ssl] [--debug] <name> <server> <port>\n")
-		os.Exit(5)
+		log.Fatal("Usage: mm [--ssl] [--debug] <name> <server> <port>")
 	}
 	connection_name = connection_args[0]
 	connection_server = connection_args[1]
 	if s, err := strconv.Atoi(connection_args[2]); err == nil {
 		connection_port = uint16(s)
 	} else {
-		fmt.Println("Port must be a number 1 - 65535")
-		os.Exit(5)
+		log.Fatal("Port must be a number 1 - 65535")
 	}
 
 	debug_log("Name:", connection_name)
@@ -49,30 +53,45 @@ func init_vars() {
 	debug_log("SSL?:", strconv.FormatBool(use_ssl))
 }
 
-func get_muck_dir() string {
-	var muck_dir string
-	if dir, err := homedir.Expand(main_dir); err == nil {
-		muck_dir = dir
-	} else {
-		fmt.Println("unable to expand home dir")
-		os.Exit(10)
-	}
-	debug_log("muckdir", muck_dir)
+func get_working_dir(base string, connection string) string {
+	var home string
 
-	return muck_dir
+	if dir, err := homedir.Dir(); err == nil {
+		home = dir
+		debug_log("Home directory", home)
+	} else {
+		log.Fatal("unable to expand home dir")
+	}
+
+	working := filepath.Join(home, base, connection)
+	return working
 }
 
-func move_to_main_directory() {
-	muck_dir := get_muck_dir()
-	if _, err := os.Stat(muck_dir); os.IsNotExist(err) {
-		if err := os.Mkdir(muck_dir); err != nil {
-			fmt.Printf("Unable to make non-existant dir %v\n", muck_dir)
-			os.Exit(10)
+func move_to_dir(directory string) {
+	if err := os.MkdirAll(directory, 0755); err != nil {
+		log.Fatalf("Unable to make connection directory %v\n", directory)
+	}
+
+	if err := os.Chdir(directory); err != nil {
+		log.Fatalf("Unable to chdir to %v\n", directory)
+	}
+}
+
+func make_in(file string) {
+	if _, err := os.Stat(file); err == nil {
+		fmt.Println("FIFO already exists. Unlink or exit")
+		fmt.Println("if you run multiple connection with the same name you're gonna have a bad time")
+		fmt.Print("Type YES to unlink and recreate: ")
+		input := bufio.NewReader(os.Stdin)
+		if answer, err := input.ReadString('\n'); err != nil || answer != "YES" {
+			log.Fatal("Canceling. Please remove FIFO before running")
+		} else {
+			syscall.Unlink(file)
 		}
 	}
-	if err := os.Chdir(muck_dir); err != nil {
-		fmt.Printf("Unable to chdir to %v\n", muck_dir)
-		os.Exit(10)
+
+	if err := syscall.Mkfifo(file, 0644); err != nil {
+		log.Fatalf("Unable to make FIFO %v", file)
 	}
 }
 
@@ -80,7 +99,11 @@ func main() {
 	//set up settings
 	init_vars()
 
-	move_to_main_directory()
+	working_dir := get_working_dir(base_dir, connection_name)
+	move_to_dir(working_dir)
+
+	make_in(in_file)
+	defer syscall.Unlink(in_file)
 	//defer clean up connection, in, and roll out
 
 	//create connection
